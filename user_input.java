da03c52eb284fec96f9bfecf33fde0bd051eb112
@abduljabbar <167172247+Abduljabbar2004@users.noreply.github.com>
2024-04-22 13:29:11 +0530

    Fix: change function variable name...

--- user_input.java
+++ user_input.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.util.Scanner;
 public class user_input {
-   void input() {
+   int input() {
     Scanner obj = new Scanner (System.in);
     String name;
     System.out.println("Enter a Student name: ");
     name = obj.nextLine();
     System.out.println("The name is: ");
+    return 0;
    }
 
     public static void main (String args[]) {
